22e938ef6b4b95cb2333ac8bb63ab443690dc5a3
marius <pakalnis.marius@gmail.com>
2017-10-09 02:48:08 +0300

    Fixing forms to accept content and write to database columns

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -21,11 +21,15 @@ class WelcomeController < ApplicationController
   end
 
   def new
-    @bunch = Bunch.find(1)
+    @customer = Customer.new
+    @order = @customer.orders.new
+    @bunch = @order.bunches.new
   end
 
   def create
-    @bunch = Bunch.new(bunch_params)
+    @customer = Customer.new(customer_params)
+    @order = @customer.orders.new(order_params)
+    @bunch = @order.bunches.new(bunch_params)
     if @bunch.save
       redirect_to root_path
       flash[:notice] = "Successfully created!"
@@ -38,7 +42,17 @@ class WelcomeController < ApplicationController
   private
 
   def bunch_params
-    params.require(:bunch).permit(:order_id, :gender_for, :comment, color:[])
+    params.require(:bunch).permit(:order_id, :gender_for, :price, :comment, color:[])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:email, :phone)
+  end
+
+  def order_params
+    params.require(:order).permit(:date, :address, :order_delivery_date,
+                                  :comment, :total_order_sum, :from,
+                                  :receiver_number, :receiver_name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#welcome'
   get '/pradinis', to: 'welcome#home', as: 'home'
-  get 'new', to: 'welcome#new'
   get 'apie_mus', to: 'welcome#about_us', as: 'about_us'
-  get 'puokstes', to: 'welcome#products', as: 'products'
-  post 'puokstes', to: "welcome#create", as: 'create'
+  get 'puokstes', to: 'welcome#new'
+  post 'puokstes', to: 'welcome#create'
 end
